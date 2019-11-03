23b003c5b232394a0454fe44839744c6bb851c91
Matheus Martins˜ <matheus.martins@ifood.com.br>
2019-11-03 17:53:05 -0300

    fix

--- packages/react-app/src/App.js
+++ packages/react-app/src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-// import Microfrontend from 'microfrontend-controller';
-
 
 // import importScript from './import-script';
 
 class App extends React.Component {
+  state = {
+    selectedMicrofrontend: null,
+    microfrontends: null
+  }
+
+  componentDidMount() {
+  }
+
+  handleMicrofrontendClick = selectedMicrofrontend => () => {
+    this.setState({
+      selectedMicrofrontend
+    });
+  }
+
   render() {
+    const { microfrontends } = this.state;
+    if (!microfrontends) return null;
     return (
       <div className="App">
-        Meu teste legal
+        <div className="App__menu">
+          {
+            Object.keys(microfrontends).map(microfrontend => (
+              <button onClick={this.handleMicrofrontendClick(microfrontend)}>
+                {microfrontend}
+              </button>
+            ))
+          }
+        </div>
       </div>
     );
   }
--- packages/react-app/src/index.js
+++ packages/react-app/src/index.js
@@ -6,6 +6,7 @@ import * as serviceWorker from './serviceWorker';
 import { ImportMicrofrontend } from 'react-microfrontend';
 
 // const { Controller } = Microfrontend;
+
 ReactDOM.render(<ImportMicrofrontend><App /></ImportMicrofrontend>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
