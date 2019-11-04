1d97e4f02b654af3e97301a7950a142e647ffa64
Luis <takahashihideki408@gmail.com>
2019-11-04 16:10:48 -0300

    Added initial ts-cra-mini-app

--- example/build-configuration.js
+++ example/build-configuration.js
@@ -1,5 +1,5 @@
 module.exports = () => ({
 	shouldBuildPackages: true,
 	app: 'react-app',
-	microfrontends: ['react-dynamic-app', 'react-another-micro-app']
+	microfrontends: ['react-dynamic-app', 'react-another-micro-app', 'ts-cra-micro-app']
 });
