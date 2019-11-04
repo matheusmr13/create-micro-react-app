0ea9cecf1d52efe1ce1643324f7ac82e784e6bbf
Luis <takahashihideki408@gmail.com>
2019-11-04 16:47:03 -0300

    Renamed to react-market-app

--- example/packages/ts-cra-micro-app/src/App.tsx
+++ example/packages/ts-cra-micro-app/src/App.tsx
@@ -8,7 +8,7 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Edit <code>src/App.AAAAA</code> and save to reload.
         </p>
         <a
           className="App-link"
