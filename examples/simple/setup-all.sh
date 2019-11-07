b7881b624e476debd1eb8e257154c2bfb834a0f5
Matheus Martins <matheusmr00@gmail.com>
2019-11-07 17:55:26 -0300

    setup all

--- examples/simple/setup-all.sh
+++ examples/simple/setup-all.sh
@@ -1,10 +1,23 @@
 lerna bootstrap
 yarn
 
-cd node_modules
+root_dir=$(pwd)
+link_deps () {
+	cd node_modules
+	rm -r microfrontend-controller
+	rm -r react-microfrontend
 
-rm -r microfrontend-controller
-rm -r react-microfrontend
+	ln -s "${root_dir}/../../packages/microfrontend-controller/" microfrontend-controller
+	ln -s "${root_dir}/../../packages/react-microfrontend/" react-microfrontend
+	cd ..
+}
 
-ln -s ../../packages/microfrontend-controller/ microfrontend-controller
-ln -s ../../packages/react-microfrontend/ react-microfrontend
+link_deps
+
+cd ./packages
+for D in `ls .`
+do
+    cd $D
+	link_deps
+	cd ..
+done
