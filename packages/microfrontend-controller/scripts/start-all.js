0df737e73a446a66b635e6515f5d50972ac5975e
Matheus Martins <matheusmr00@gmail.com>
2019-11-19 09:41:40 -0800

    test

--- packages/microfrontend-controller/scripts/start-all.js
+++ packages/microfrontend-controller/scripts/start-all.js
@@ -19,6 +19,7 @@ const startReactApp = async (package, port, isMicro) => {
 	await promiseExec(`${isMicro ? 'REACT_APP_IS_MICROFRONTEND=true ' : ''}PORT=${port} npm run --prefix ./packages/${package} start`);
 }
   
+
 const startAll = async () => {
 	if (!microfrontends || !app) throw new Error('Configuration "microfrontends" and "app" are required.');
 
--- packages/react-microfrontend/src/index.js
+++ packages/react-microfrontend/src/index.js
@@ -3,3 +3,4 @@ export { default as Observable } from './observable';
 
 export { default as ImportMicrofrontend, withMicrofrontend } from './import-microfrontend';
 export { default as ExportMicrofrontend } from './expose-microfrontend';
+
