57acca11f32fcd162bd6b58a8ccb13dc9ac5e406
Gustavo Taminato <gustavo.taminato@ifood.com.br>
2020-01-02 17:57:00 -0300

    Rename packageName to market_quick-cart

--- examples/market/packages/quick-cart/build-lib/index.js
+++ examples/market/packages/quick-cart/build-lib/index.js
@@ -1,9 +1,9 @@
 
-		import { CreateLib } from 'react-microfrontend';
-		import schema from './schema';
+    import { CreateLib } from 'react-microfrontend';
+    import schema from './schema';
 
-		export default CreateLib(schema, {
-			apiAccess: CreateLib.BUILD_TYPE.PUBLIC_API,
-			packageName: "@market/quick-cart"
-		});
-	
+    export default CreateLib(schema, {
+      apiAccess: CreateLib.BUILD_TYPE.PUBLIC_API,
+      packageName: "market_quick-cart"
+    });
+  
