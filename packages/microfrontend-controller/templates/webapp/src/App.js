bb19165f45362d8a368b26376ed5e30e46c517f2
Luis <takahashihideki408@gmail.com>
2020-01-21 19:31:47 -0300

    Added hooks to initial app template

--- packages/microfrontend-controller/templates/webapp/src/App.js
+++ packages/microfrontend-controller/templates/webapp/src/App.js
@@ -1,55 +1,46 @@
 import { withMicrofrontend } from 'react-microfrontend';
 
-import React from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
 
-class App extends React.Component {
-  state = {
-    selectedMicrofrontend: null
-  }
+const App = ({ microfrontends }) => {
+  const [selectedMicrofrontendKey, setSelectedMicrofrontendKey] = useState(undefined);
 
-  handleMicrofrontendClick = selectedMicrofrontend => () => {
-    this.setState({
-      selectedMicrofrontend
-    });
-  }
+  const MicrofrontendComponent = selectedMicrofrontendKey
+    ? microfrontends[selectedMicrofrontendKey]
+    : Object.values(microfrontends)[0];
 
-  render() {
-    const { microfrontends } = this.props;
-    const { selectedMicrofrontend } = this.state;
-
-    const MicrofrontendComponent = selectedMicrofrontend && microfrontends[selectedMicrofrontend].view;
-    
-    return (
-      <div className="App">
-        <p className="App__header">
-          Welcome to Microfrontend World.<br/>
-          Choose a microfrontend to show:
-        </p>
-        <div className="App__menu">
-          {
-            Object.keys(microfrontends).map(microfrontend => (
-              <button
-                onClick={this.handleMicrofrontendClick(microfrontend)}
-                key={microfrontend}
-                className={`App__menu-item${microfrontend === selectedMicrofrontend ? ' App__menu-item--selected' : ''}`}
-              >
-                {microfrontend}
-              </button>
-            ))
-          }
-        </div>
-        <div className="App__content">
-          {
-            MicrofrontendComponent && (
-              <div className="App__microfrontend-content">
-                <MicrofrontendComponent />
-              </div>
-            )
-          }
-        </div>
+  return (
+    <div className="App">
+      <p className="App__header">
+        Welcome to Microfrontend World.
+        <br />
+        Choose a microfrontend to show:
+      </p>
+      <div className="App__menu">
+        {
+          Object.keys(microfrontends).map(microfrontend => (
+            <button
+              onClick={() => setSelectedMicrofrontendKey(microfrontend)}
+              key={microfrontend}
+              className={`App__menu-item${microfrontend === selectedMicrofrontendKey ? ' App__menu-item--selected' : ''}`}
+            >
+              {microfrontend}
+            </button>
+          ))
+        }
       </div>
-    );
-  }
-}
+      <div className="App__content">
+        {
+          MicrofrontendComponent && (
+            <div className="App__microfrontend-content">
+              <MicrofrontendComponent.view />
+            </div>
+          )
+        }
+      </div>
+    </div>
+  );
+};
 export default withMicrofrontend(App);
