e4179a53354b1835551a4f1b5abb8b36ed2c5e6a
Luis <takahashihideki408@gmail.com>
2020-03-05 13:31:39 -0300

    removed the trhow at the try and used a let instead

--- packages/react-microfrontend/src/fetch-retry.js
+++ packages/react-microfrontend/src/fetch-retry.js
@@ -2,6 +2,7 @@ const LIMIT = 1;
 
 const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 const fetchRetry = async (url, { limit = LIMIT, delay, ...opts } = {}) => {
+  let error;
   try {
     const response = await fetch(url, opts);
     const { status, statusText } = response;
@@ -10,17 +11,17 @@ const fetchRetry = async (url, { limit = LIMIT, delay, ...opts } = {}) => {
       return response.json();
     }
 
-    throw new Error(statusText)
-  } catch (error) {
-    const shouldRetry = limit > 0;
-
-    if (!shouldRetry) throw error;
-
-    if (delay) await sleep(delay);
-
-    return await fetchRetry(url, { limit: limit - 1, delay, ...opts });
+    error = new Error(statusText);
+  } catch (_err) {
+    error = _err;
   }
-}
 
+  const shouldRetry = limit > 0;
+  if (!shouldRetry) throw error;
+
+  if (delay) await sleep(delay);
+
+  return await fetchRetry(url, { limit: limit - 1, delay, ...opts });
+}
 
 export default fetchRetry
