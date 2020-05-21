e0826945e2f215cbd2049a092755054ffa4f47be
Matheus Martins <matheusmr00@gmail.com>
2020-05-21 16:56:06 -0300

    Updates

---
+++ app/precache-manifest.abcf77a20f0e4a22b94274afeb417b58.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eaee3554c41b1ef75d6ecbdfb1d4560f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f6af91c0bb709615e25f",
+    "url": "./static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "c33ae4e91e32af51dcaf",
+    "url": "./static/css/main.798f63cd.chunk.css"
+  },
+  {
+    "revision": "f6af91c0bb709615e25f",
+    "url": "./static/js/2.13e102cb.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.13e102cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c33ae4e91e32af51dcaf",
+    "url": "./static/js/main.14c86570.chunk.js"
+  },
+  {
+    "revision": "8d73c22166bfb0401e91",
+    "url": "./static/js/runtime-main.b3fc422b.js"
+  }
+]);
