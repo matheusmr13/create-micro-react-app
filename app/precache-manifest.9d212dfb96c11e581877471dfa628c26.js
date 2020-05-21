a47a794ba5626ae1e5645b7fcabcbe34ca6ab357
Matheus Martins <matheusmr00@gmail.com>
2020-05-21 18:05:52 -0300

    Updates

---
+++ app/precache-manifest.9d212dfb96c11e581877471dfa628c26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de09e1a7c6f682e0b581861563a42852",
+    "url": "/create-micro-react-app/app/index.html"
+  },
+  {
+    "revision": "94aca8e60fae79532fe4",
+    "url": "/create-micro-react-app/app/static/css/2.4c6ee7a4.chunk.css"
+  },
+  {
+    "revision": "f31ddee2b08edfa4ff61",
+    "url": "/create-micro-react-app/app/static/css/main.798f63cd.chunk.css"
+  },
+  {
+    "revision": "94aca8e60fae79532fe4",
+    "url": "/create-micro-react-app/app/static/js/2.b2a643e3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/create-micro-react-app/app/static/js/2.b2a643e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f31ddee2b08edfa4ff61",
+    "url": "/create-micro-react-app/app/static/js/main.612df19e.chunk.js"
+  },
+  {
+    "revision": "09d66a96624270bdaa8f",
+    "url": "/create-micro-react-app/app/static/js/runtime-main.4003bef4.js"
+  }
+]);
